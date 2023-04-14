27d98574947709a182f3869e2caabe254165f3d6
Maciej <maciej.kubale@gmail.com>
2023-04-14 19:01:23 +0200

    reviewsIds are not Ids but Reviews

--- src/main/java/pl/mk/movies/models/Movie.java
+++ src/main/java/pl/mk/movies/models/Movie.java
@@ -27,5 +27,5 @@ public class Movie {
     private List<String> genres;
     private List<String> backdrops;
     @DocumentReference
-    private List<Review> reviewsIds;
+    private List<Review> reviews;
 }
